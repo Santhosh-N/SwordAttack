0e041a4a789ed7d8c86abf85eec299e57603e673
Santhosh-N <59107079+Santhosh-N@users.noreply.github.com>
2020-07-21 21:30:06 +0530

    Add files via upload

---
+++ EnemyDestroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDestroy : MonoBehaviour
+{
+    public GameObject EnemyDestroyEffect;
+   // public GameObject HealthFollowParticle;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+		if (col.gameObject.tag == "SwordAttackPoint")
+		{
+			 SoundEffect.PlaySound("enemydeath");
+			 Instantiate(EnemyDestroyEffect, transform.position, Quaternion.identity);
+			 Destroy(gameObject);
+		}
+        if(col.gameObject.tag == "Bullet"  ) //|| col.gameObject.tag == "Player"
+        {
+            SoundEffect.PlaySound("enemydeath");
+            Instantiate(EnemyDestroyEffect, transform.position, Quaternion.identity);
+           // Instantiate(HealthFollowParticle, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
